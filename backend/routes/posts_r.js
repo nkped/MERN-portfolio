ff5a615af83a1ab9552b50d4f9177f0eef1f5e97
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-02-06 16:38:10 +0100

    clean up

--- backend/routes/posts_r.js
+++ backend/routes/posts_r.js
@@ -5,27 +5,12 @@ import { createPost, getOnePost, getAllPosts, deletePost, updatePost } from '../
 
 const router = express.Router()
 
-
-//Get all posts
 router.get('/', getAllPosts)
-
-
-//Get One post
 router.get('/:id', getOnePost)
-
-//Create post
 router.post('/', createPost)
-
-
 router.delete('/:id', deletePost)
-
 router.patch('/:id', updatePost)
 
-/*
-router.get('/', (req, res) => {})
-router.get('/', (req, res) => {})
-router.get('/', (req, res) => {})
-*/
 
 export default router
 
