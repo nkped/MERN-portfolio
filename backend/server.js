fb64fa1455cb22d6aba9391b0525e56f113ffbb6
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-02-16 20:13:01 +0100

    add cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 import posts from './routes/posts_r.js'
 
@@ -9,6 +10,7 @@ import posts from './routes/posts_r.js'
 dotenv.config()
 const app = express()
 
+app.use(cors())
 
 app.use(express.json())
 app.use(( req, res, next) => {
